ad2529e66e18474bcb641649cd8a3216151ec607
Lauren <42990294+LawenXx@users.noreply.github.com>
2021-03-06 00:37:41 +0000

    pt3

---
+++ Black Ops 2/Tuts/Tuts/Structs.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#define ScrPlacement 0x82CBC168
+
+struct Material
+{
+	const char* material;
+};
+
+struct Font
+{
+	const char* name;
+	int pixelheight;
+};
+
+struct Vector3 {
+	float x, y, z;
+};
+
+struct UIContext
+{
+	int32_t contextid; //0x0000
+	char pad_0004[24]; //0x0004
+	int32_t ScreenWidth; //0x001C
+	int32_t ScreenHeight; //0x0020
+	char pad_0024[8]; //0x0024
+};
+
+struct Centity
+{
+	char pad_0000[44]; //0x0000
+	Vector3 Origin; //0x002C
+	Vector3 ViewAngles; //0x0038
+	char pad_0044[272]; //0x0044
+	int32_t Flags; //0x0154
+	char pad_0158[88]; //0x0158
+	int32_t PrimaryWeapon; //0x01B0
+	int32_t SecondaryWepon; //0x01B4
+	char pad_01B8[144]; //0x01B8
+	uint16_t N0000034C; //0x0248
+	uint16_t N0000038A; //0x024A
+	char pad_024C[92]; //0x024C
+	uint16_t eType; //0x02A8
+	char pad_02AA[206]; //0x02AA
+};
+
+extern Centity* cg_entitiesArray;
+extern UIContext* CGDC;
